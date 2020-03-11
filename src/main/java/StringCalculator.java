23d30462449f9841cacc61d95aa1c43177be44f3
Dionne <dionne.condorfarrell@tfl.gov.uk>
2020-03-11 09:54:22 +0000

    added new feature for arbitary numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,29 +2,26 @@ public class StringCalculator {
 
     public int add(String numbers) {
 
-        if(numbers.length() < 2) {
+        if (numbers == "") {
 
-            if ("".equals(numbers)) {
-                return 0;
-            }
-
-            return Integer.valueOf(numbers);
-
+            return 0;
         }
-
 
         String[] arraySplit = numbers.split(",");
 
+        if (arraySplit.length == 1) {
+
+            return Integer.valueOf(arraySplit[0]);
+        }
+
         int total = 0;
 
-        for(int i = 0; i < arraySplit.length; i ++){
+        for (int i = 0; i < arraySplit.length; i++) {
             int item = Integer.valueOf(arraySplit[i]);
             total += item;
 
         }
 
         return total;
-
-
     }
 }
--- src/test/java/StringCalculatorMust.java
+++ src/test/java/StringCalculatorMust.java
@@ -54,4 +54,21 @@ public class StringCalculatorMust {
         Assert.assertEquals(3,result);
     }
 
+    @Test
+
+    public void returnIntForArbitraryNumberSize() {
+
+        //Arrange
+
+        StringCalculator stringcalculator = new StringCalculator();
+
+        //Act
+        int result = stringcalculator.add("1,2,3,4,5,6,7,8,9");
+
+        //Assert
+
+        Assert.assertEquals(45, result);
+
+    }
+
 }
